82e2ec49eb6c886f45fa4f090982132c3e9890d7
Sergio Martin Rubio <econsergio@gmail.com>
2021-07-27 19:52:39 +0200

    Add super equals and hashcode

--- src/main/java/com/sergiomartinrubio/model/Message.java
+++ src/main/java/com/sergiomartinrubio/model/Message.java
@@ -3,11 +3,13 @@ package com.sergiomartinrubio.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class Message extends Result {
     @JsonProperty("message_id")
     private long messageId;
