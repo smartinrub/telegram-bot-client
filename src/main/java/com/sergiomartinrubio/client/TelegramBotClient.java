6c4e87a977f378a016eb5141492ca9fd8437514f
Sergio Martin Rubio <econsergio@gmail.com>
2021-07-30 12:02:56 +0200

    Update Java docs

--- src/main/java/com/sergiomartinrubio/client/TelegramBotClient.java
+++ src/main/java/com/sergiomartinrubio/client/TelegramBotClient.java
@@ -1,7 +1,6 @@
 package com.sergiomartinrubio.client;
 
 import com.sergiomartinrubio.model.Message;
-import com.sergiomartinrubio.model.Response;
 import com.sergiomartinrubio.model.User;
 
 /**
@@ -14,24 +13,32 @@ public interface TelegramBotClient {
     /**
      * Sends a message to a particular chat
      *
+     * @param chatId  the target chat for the bot message
+     * @param message the message as {@link String}
+     * @return information about the message sent
      * @see <a href="https://core.telegram.org/bots/api#sendmessage">Telegram SendMessage Doc</a>
      * @see <a href="https://core.telegram.org/bots/api#message">Telegram Returned Message Doc</a>
-     *
-     * @param chatId the target chat for the bot message
-     * @param message the message as {@link String}
-     * @return message response if successful
      */
     Message sendMessage(long chatId, String message);
 
     /**
      * Returns basic information about the bot
      *
+     * @return the bot in form of a {@link User}
      * @see <a href="https://core.telegram.org/bots/api#getme">Telegram GetMe Doc</a>
      * @see <a href="https://core.telegram.org/bots/api#user">Telegram Returned User Doc</a>
-     *
-     * @return the bot in form of a {@link User}
      */
     User getMe();
 
+    /**
+     * Forwards message from one chat to another
+     *
+     * @param chatId     the destination chat for the message to be forwarded
+     * @param fromChatId the source chat where the message to be forwarded resides
+     * @param messageId  the target message to be forwarded
+     * @return information about the message forwarded
+     * @see <a href="https://core.telegram.org/bots/api#forwardmessage">Telegram ForwardMessage Doc</a>
+     * @see <a href="https://core.telegram.org/bots/api#message">Telegram Returned Message Doc</a>
+     */
     Message forwardMessage(long chatId, long fromChatId, long messageId);
 }
